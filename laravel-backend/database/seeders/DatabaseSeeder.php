12507d9d4feae446c4cba34c1a6fa3a54b823e18
Dezső Tókos <dezsotokos@minic.ro>
2025-09-29 22:35:48 +0300

    extend DatabasSeeder.php with PopularMovieSeeder

--- laravel-backend/database/seeders/DatabaseSeeder.php
+++ laravel-backend/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             GenreSeeder::class,
             MovieSeeder::class,
             MovieListSeeder::class,
+            PopularMovieSeeder::class,
         ]);
     }
 }
