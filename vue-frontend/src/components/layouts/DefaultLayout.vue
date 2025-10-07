7dd2af142d251ca7d02cec1f281aabc6599171c3
Dezső Tókos <dezsotokos@minic.ro>
2025-10-07 20:37:57 +0300

    add new icons, logo

--- vue-frontend/src/components/layouts/DefaultLayout.vue
+++ vue-frontend/src/components/layouts/DefaultLayout.vue
@@ -5,7 +5,7 @@
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
             <div class="shrink-0">
-              <img class="size-8" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+              <img class="h-9 w-auto" src="/movie.svg" alt="CineShelf" />
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
